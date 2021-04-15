1faf690710895137488f315498c1679798867623
Brian Douglas <bdougie@users.noreply.github.com>
2021-04-15 09:34:56 -0400

    moar commands

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
     ComfyJS.Say( "https://discord.com/invite/U2peSNf23P" );
   }
   
-  if( command === "youtube" ) {
+  if( command === "youtube" || command === "yt" ) {
     ComfyJS.Say( "https://youtube.com/bdougie" );
   }
   
@@ -29,6 +29,14 @@ ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
   if( command === "swag" ) {
     ComfyJS.Say( "https://swag.opensauced.pizza" );
   }
+  
+  if( command === "coc" ) {
+    ComfyJS.Say( "The code of conduct: https://github.com/open-sauced/code-of-conduct" );
+  }
+  
+  if( command === "commands" ) {
+    ComfyJS.Say( "!github, !title, !yo, !twitter, !coc, !yt, !sauced, !twitter" );
+  }  	
   
   if( (flags.mod || flags.broadcaster) && command === "so" ) {
     ComfyJS.Say( `everyone go follow https://twitch.tv/${message}` );
