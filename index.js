fe1e58850d75d3049c5288e01cc75d1dfd9c1b86
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2021-04-14 21:37:03 -0400

    🍕

---
+++ index.js
@@ -0,0 +1,13 @@
+const ComfyJS = require("comfy.js");
+require('dotenv').config()
+
+ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
+  if( command === "test" ) {
+    ComfyJS.Say( "replying to !test" );
+  }
+
+  if( (flags.mod || flags.broadcaster) && command === "so" ) {
+    ComfyJS.Say( "everyone go follow https://twitch.tv/${message}" );
+  }
+}
+ComfyJS.Init( process.env.TWITCHUSER, process.env.OAUTH );
